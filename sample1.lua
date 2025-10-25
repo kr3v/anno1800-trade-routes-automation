e9ce32534689417b0f80800a776e69626efb0aee
Dmitry Baynak <dbaynak@protonmail.com>
2025-10-25 22:23:06 +0300

    get all ships

--- sample1.lua
+++ sample1.lua
@@ -5,17 +5,31 @@ package.loaded["lua/serp/lighttools"] = nil;
 local inspect_object_yaml = require("lua/inspector");
 local logger = require("lua/logger");
 local serpLight = require("lua/serp/lighttools");
+local objectAccessor = require("lua/object_accessor");
 
 local function GetShipCargo_ith()
     return
 end
 
 local function GetShipCargo(oid)
-    inspect_object_yaml(serpLight, "SerpLightModule");
     return serpLight.GetVectorGuidsFromSessionObject(
             '[MetaObjects SessionGameObject(' .. tostring(oid) .. ') ItemContainer Cargo Count]',
             { Guid = "string", Value = "string" }
     );
+end
+
+local function GetAllShips()
+    return serpLight.GetCurrentSessionObjectsFromLocaleByProperty("Walking");
+end
+
+local function GetAllShipsNames()
+    local ships = GetAllShips();
+    local ship_names = {};
+    for oid, ship in pairs(ships) do
+        local name = objectAccessor(oid, {}).Nameable.Name;
+        ship_names[oid] = name;
+    end
+    return ship_names;
 end
 
 ------------------------------------------------
@@ -34,8 +48,8 @@ local oid = tonumber(obj_str:match("oid (%d+)"))
 print("------------------------------------------------")
 
 local success, err = pcall(function()
-    local val = GetShipCargo(oid);
-    inspect_object_yaml(val, "CargoItems");
+    local val = GetAllShipsNames(oid);
+    inspect_object_yaml(val, "GetAllShipsNames");
 end);
 print("PCALL success: " .. tostring(success));
 print("PCALL error: " .. tostring(err));
